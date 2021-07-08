a552e5f081ec006ee8d984d5cef944c8f05d3659
Berigos <83034164+Berigos@users.noreply.github.com>
2021-07-08 19:33:26 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Вход</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action ="" method="">
+    <label>Авторизация</label>
+    <input type="text" style="background:url(photo/key-xxl.png)  no-repeat;  background-position:left; background-size:12% 80%;" placeholder="Логин">
+    <input type="password" placeholder="Пароль">
+    <button>Продолжить</button>
+    </form>
+</body>
+</html>
